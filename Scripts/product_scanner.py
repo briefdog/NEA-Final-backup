6a78a45d7ef85fca99482efa2b66504edbc26516
briefdog <121690859+briefdog@users.noreply.github.com>
2024-03-11 12:09:34 +0000

    Update product_scanner.py

--- Scripts/product_scanner.py
+++ Scripts/product_scanner.py
@@ -22,8 +22,7 @@ from io import BytesIO
 import os
 
 #the image is displayed bigger when it is converted into tkinter format than in customtkinter,
-#so warning messages may come up as I am using the tkinter format in customtkinter, even though it works
-
+#so warning messages may come up in the terminal as I am using the tkinter format in customtkinter, even though it works
 
 #import subprocess for switching between python scripts
 import subprocess
@@ -50,13 +49,19 @@ class Button(ctk.CTkButton):
         super().__init__(master, text=text, command=command)
         self.place(**kwargs)
 
+#class to create image label
+class ImageLabel(ctk.CTkLabel):
+    def __init__(self, master, text, **kwargs):
+        super().__init__(master, text=text)
+        self.place(**kwargs)
+
 #class for window to run
 class window(ctk.CTk):
     def __init__(self):
         super().__init__()
 
         self.title("FitPro")
-        self.geometry("820x1000")
+        self.geometry("820x600")
         self.resizable(False, False)
         #makes font "bold"
         self.title_font = Font("Helvetica", 20, "bold")
@@ -65,6 +70,9 @@ class window(ctk.CTk):
         self.title_label = Label(self, "Enter product code here:", x=100, y=50, font=self.title_font)
         self.button = Button(self, "Find information", self.getfoodinfo, x=100, y=150)
         self.button = Button(self, "Go back to homepage", self.goback, x=10,y=10)
+        #image label created so it can be configured in the fetch_and_display_image label method to display an image
+        self.image_label = ImageLabel(master=self, text="", x=500, y=200)
+
         # text variable
         self.productcode = StringVar()
         self.entry = Entry(self, "", self.productcode, x=100, y=100)
@@ -75,6 +83,7 @@ class window(ctk.CTk):
         url = f"https://world.openfoodfacts.org/api/v0/product/{product_input}.json"    
         try:
             response = requests.get(url)
+        #if there is no internet connection, error box will appear
         except:
             messagebox.showerror(title = "Error", message = "Network error\nMake sure to check your internet connection")
             return
@@ -95,41 +104,32 @@ class window(ctk.CTk):
                         if isinstance(widget, Label) and widget != self.title_label:
                             widget.destroy()
 
-                    #Get data from dictionaries and then display the data as labels, I used json data dump in another file to help find the dictionaries with data
-                    product_name_label = Label(self, f"Product name: {product_info.get('product_name', 'N/A')}",font=("Helvetica", 16))
-                    product_name_label.place(x=100,y=200)
+                    #Get data from dictionaries and then display the data as labels, 
+                    #I used json data dump in another file, and documentation to help find the dictionaries with data
+                    product_name_label = Label(self, f"Product name: {product_info.get('product_name', 'N/A')}",font=("Helvetica", 16),x=100,y=200)
 
-                    brand_label = Label(self, f"Brand: {product_info.get('brands', 'N/A')}", font=("Helvetica", 16))
-                    brand_label.place(x=100,y=230)
+                    brand_label = Label(self, f"Brand: {product_info.get('brands', 'N/A')}", font=("Helvetica", 16),x=100,y=230)
 
-                    categories_label = Label(self, f"Categories: {', '.join(product_info.get('categories_tags', ['N/A'])).split(' ')[2]}",font=("Helvetica", 16))
-                    categories_label.place(x=100,y=260)
+                    categories_label = Label(self, f"Categories: {', '.join(product_info.get('categories_tags', ['N/A'])[:2])}", font=("Helvetica", 16), x=100, y=260)
 
-                    allergens_label = Label(self, f"Allergens: {', '.join(product_info.get('allergens_tags', ['N/A']))}",font=("Helvetica", 16))
-                    allergens_label.place(x=100,y=290)
+                    allergens_label = Label(self, f"Allergens: {', '.join(product_info.get('allergens_tags', 'N/A'))}",font=("Helvetica", 16),x=100,y=290)
 
-                    nutrition_info_label = Label(self, "Nutrition Information:", font=("Helvetica", 16))
-                    nutrition_info_label.place(x=100,y=320)
+                    nutrition_info_label = Label(self, "Nutrition Information:", font=("Helvetica", 16),x=100,y=320)
 
-                    energy_label = Label(self, f"Energy: {nutrition_info.get('energy-kcal_100g', 'N/A')} kcal/100g",font=("Helvetica", 16))
-                    energy_label.place(x=100,y=350)
+                    energy_label = Label(self, f"Energy: {nutrition_info.get('energy-kcal_100g', 'N/A')} kcal/100g",font=("Helvetica", 16),x=100,y=350)
 
-                    fat_label = Label(self, f"Fat: {nutrition_info.get('fat_100g', 'N/A')} g/100g", font=("Helvetica", 16))
-                    fat_label.place(x=100,y=380)
+                    fat_label = Label(self, f"Fat: {nutrition_info.get('fat_100g', 'N/A')} g/100g", font=("Helvetica", 16),x=100,y=380)
 
-                    carbohydrates_label = Label(self,f"Carbohydrates: {nutrition_info.get('carbohydrates_100g', 'N/A')} g/100g",font=("Helvetica", 16))
-                    carbohydrates_label.place(x=100,y=410)
+                    carbohydrates_label = Label(self,f"Carbohydrates: {nutrition_info.get('carbohydrates_100g', 'N/A')} g/100g",font=("Helvetica", 16),x=100,y=410)
 
-                    proteins_label = Label(self, f"Proteins: {nutrition_info.get('proteins_100g', 'N/A')} g/100g",font=("Helvetica", 16))
-                    proteins_label.place(x=100,y=440)
+                    proteins_label = Label(self, f"Proteins: {nutrition_info.get('proteins_100g', 'N/A')} g/100g",font=("Helvetica", 16),x=100,y=440)
                 
-                    nutri_score = Label(self, f"Nutrition Score: {nutrition_score.get('grade', 'N/A')}",font=("Helvetica", 16))
-                    nutri_score.place(x=100,y=470)
+                    nutri_score = Label(self, f"Nutrition Score: {nutrition_score.get('grade', 'N/A')}",font=("Helvetica", 16),x=100,y=470)
                     #go to display image method
                     self.fetch_and_display_image()
                 #show error messages if there are any problems connecting to the api or retrieving the information
                 else:
-                    messagebox.showerror(title = "error", message = "Product not found.")
+                    messagebox.showerror(title = "error", message = "Product not found")
             except requests.exceptions.JSONDecodeError as e:
                 messagebox.showerror(title = "Error", message = f"Error decoding JSON: {e}\n{response.content}")
         else:
@@ -144,14 +144,12 @@ class window(ctk.CTk):
                 image_data = response.content
                 #create PIL image from binary data
                 img = Image.open(BytesIO(image_data))
-                #create and place label so image can be shown
-                image_label = ctk.CTkLabel(master = self,text = "")
-                image_label.place(x=500,y=200)
                 #convert the PIL image to tkinter format
                 #This video was used to help with pillow and ImageTk https://www.youtube.com/watch?v=kjc53i4xUmw
                 photo = ImageTk.PhotoImage(img)
                 #change label so it displays image
-                image_label.configure(image=photo)
+                self.image_label.configure(image=photo)
+                self.image_label.image = photo
             
             else:
                 messagebox.showerror(title = "Error",message="Failed to fetch image")
